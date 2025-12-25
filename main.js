f863e05b6d564330b81d9593d7f95afe3ea4934b
Vutan <tanvukt2003@gmail.com>
2025-12-25 15:38:46 +0700

    feat: update message

---
+++ main.js
@@ -0,0 +1,12 @@
+const socket = io();
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value) {
+        socket.emit("chat message", input.value);
+        input.value = "";
+    }
+});
